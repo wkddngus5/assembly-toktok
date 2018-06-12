2815c344d13a9198ead031b2040dad9c1d8c0d23
nimorrna@hanmail.net <rnlwjd0613@gmail.com>
2018-06-12 09:07:27 +0900

    API HTTP GET project/{id} 추가

---
+++ spring-boot/src/main/java/controller/ApiProjectController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import dao.ProjectDao;
+import domain.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ApiProjectController {
+    @Autowired
+    private ProjectDao projectDao;
+
+    @RequestMapping(value = "/projects/{id}", method = RequestMethod.GET)
+    @ResponseStatus( HttpStatus.OK )
+    public ResponseEntity<Project> post(@PathVariable("id") final Long id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        Project project = projectDao.findById(id).orElse(null);
+        if(project == null){
+            return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
+        }else {
+            return new ResponseEntity<>(project, headers, HttpStatus.OK);
+        }
+    }
+}
