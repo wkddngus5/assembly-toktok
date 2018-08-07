2e9005c17861587241949742a073d4c56745d12c
wkddngus5 <wkddngus5@naver.com>
2018-08-07 18:30:49 +0900

    polyfill foreach

--- spring-boot/src/main/resources/public/entry/main.js
+++ spring-boot/src/main/resources/public/entry/main.js
@@ -1,6 +1,7 @@
 import carousel from '../js/carousel';
 import header from '../js/header';
 import mainProjects from '../js/mainProjects';
+import 'nodelist-foreach-polyfill';
 
 (function () {
   new carousel();
